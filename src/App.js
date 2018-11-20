4392131a96b257f23baf0b83e94c03cd8239ba0f
garcila <germanarcila@gmail.com>
2018-11-19 22:06:40 -0500

    fixed counter and added 20 items to verbs list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import WOW from "wow.js";
+import React, { useState, useEffect, useRef } from 'react';
+import WOW from "wow.js"; // animation
 import './App.css';
+
+import { verbs, shiftDURATION } from './data';
+
+var moment = require('moment'); // time management
 
 function App() {
   const [count, setCount] = useState(0);
-  const verbs = ['ghost','weightless','translucent','spark','krill','here','hungry','present','above','future','obsolete!'];
+  const [left, setLeft] = useState(20);
+  const [start, setStart] = useState();
 
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
     // Update the document title using the browser API
     document.title = `You clicked ${count} times`;
     
+    // creates a new animation object and initializes it.
     const wow = new WOW();
     wow.init();
   });
 
-  function handleClick() {
-    setCount(count + 1);
+  async function handleClick() {
+    if(count === 0) {
+      await setStart(moment().unix());
+      await setCount(1, setLeft(20 - 1));
+    } else {
+      setCount(count + 1, setLeft(left - 1));
+    }
   }
+
+  let warn = `time left ${shiftDURATION - left}`
 
   return (
     <div className='App'>
@@ -30,11 +43,13 @@ function App() {
           {count}
         </p>
         <div className="verb">
-          <p className='verb-text'>{count < 11 ? verbs[count] : verbs[10]}</p>
+          <p className='verb-text'>{count < 21 ? verbs[count] : verbs[20]}</p>
         </div>
       </div>
+      {/* <p>{warn}</p> */}
     </div>
   );
 }
 
 export default App;
+
---
+++ src/data.js
@@ -0,0 +1,4 @@
+export const verbs = ['ghost','weightless','translucent','spark','krill','here','hungry','past','present','fly','pebble',
+'pyro','sitting','lucky','mid teen','walking','close',' not above','future','there','obsolete!'];
+
+export const shiftDURATION = 57600; // 4 hours in seconds
